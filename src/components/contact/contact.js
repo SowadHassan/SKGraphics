55abf35534f571aef707fc7d199588c3c17bcaa0
SowadHassan <sowadhassan@gmail.com>
2020-09-04 21:11:02 +0600

    aaaaa

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,8 +1,10 @@
 import React from "react";
+import TopTitle from "../../layout/top_tilte/top_title";
 
 function Contact() {
   return (
     <>
+      <TopTitle />
       <p>Contact</p>
     </>
   );
--- src/components/demos/demos.js
+++ src/components/demos/demos.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
+import TopTitle from "../../layout/top_tilte/top_title";
 
 function Demos() {
   return (
     <>
-      <p>Demos</p>
+      <TopTitle />
+      <br/>
+      <Card style={{ marginLeft: '20px', width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          style={{ width: "300px", height: "300px" }}
+          src="https://www.docker.com/sites/default/files/social/docker_facebook_share.png"
+        />
+        <Card.Body>
+          <Card.Title>Docker</Card.Title>
+          {/* <Card.Text>Docker Updatge</Card.Text>
+          <Button variant="primary">Update</Button> */}
+        </Card.Body>
+      </Card>
     </>
   );
 }
